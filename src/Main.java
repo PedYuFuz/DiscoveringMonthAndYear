6fe2b334d95b40c59cdd92db393f1e79fd553d33
PedYuFuz <62225261+PedYuFuz@users.noreply.github.com>
2021-07-08 09:07:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,20 @@ public class Main {
         Scanner monthScan = new Scanner(System.in);
         System.out.print("\nType the month you want: ");
 
-        //read and save the month
+        //Read and save the month
         int month = monthScan.nextInt();
 
+        //Make sure the month is available
         if(month < 0 || month > 12){
             month = 0;
             monthScan = new Scanner(System.in);
             System.out.print("\nType a correct month: ");
             month = monthScan.nextInt();
+        //If it's a leap year and february as the month...
         }else if(year%4==0 && month==2){
             System.out.println("\nIt's a leap year!");            
-            System.out.println("So... February has 29 days");            
+            System.out.println("So... February has 29 days"); 
+        //If its not a leap year...
         }else{
             if(month==4 || month==6 || month==9 || month==11){
                 System.out.println("This month has 30 days.");
